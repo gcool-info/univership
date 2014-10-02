80f56ffe1ae1704672f0594fe06487cbb5d96e8b
gcool-info <george.koulouris1@gmail.com>
2014-10-02 12:34:36 +0200

    Created header which appears / disappears

---
+++ client/views/home.js
@@ -0,0 +1,3 @@
+Template.home.rendered = function() { 
+	$('.home').fullpage( { menu: true });
+}
---
+++ client/views/includes/header.js
@@ -0,0 +1,31 @@
+Template.header.rendered = function() { 
+
+	var showHeader = true;
+	var t = setTimeout(hideHeader, 5000);
+
+	function hideHeader() {
+		var height = $(".header").height();
+		$(".header").animate({top: -height},500);
+		console.log("running");
+		if (!showHeader) {
+			clearTimeout(t);
+			showHeader = true;
+		} else {
+			showHeader = false;
+		}
+	}
+
+	$("body").click(function(){
+		if (showHeader) {
+			clearTimeout(t);
+			var height = $(".header").height();
+			$(".header").animate({top: -height},500);
+			showHeader = false;
+		} else {
+			clearTimeout(t);
+			$(".header").animate({top: 0},500);
+			showHeader = true;
+			t = setTimeout(hideHeader, 5000);
+		}
+	});
+}
