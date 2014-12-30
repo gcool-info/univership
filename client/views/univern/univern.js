1f9865018098398393a22a67f0c7b5382b724308
gcool-info <george.koulouris1@gmail.com>
2014-12-30 19:29:40 +0100

    Added upload btns for video and main page

--- client/views/univern/univern.js
+++ client/views/univern/univern.js
@@ -37,7 +37,26 @@ Template.univern.helpers({
 		if (!currentUser)
 			return;
 
-		return (currentUser.profile.video !== '' ? currentUser.profile.video : false);
+		var videoURL = currentUser.profile.video;
+
+		if (videoURL == '')
+			return false;
+
+		/* Regular expression to get the video key (only youtube is supported for now) */
+		var videoRegEx = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
+
+		var videoKey = videoURL.match(videoRegEx)[1];
+
+		return '//www.youtube.com/embed/' + videoKey + '?rel=0&amp;showinfo=0';
+	},
+
+	isOwner: function() {
+		var currentUser = Meteor.users.findOne();
+
+		if (!currentUser)
+			return;
+
+		return (Meteor.user() == currentUser ? true : false);
 	}
 })
 
