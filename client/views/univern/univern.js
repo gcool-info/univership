235573154cf4a701677c8d4472755aa86606bbf3
gcool-info <george.koulouris1@gmail.com>
2014-12-30 19:04:09 +0100

    Added carousel on univern page

--- client/views/univern/univern.js
+++ client/views/univern/univern.js
@@ -1,2 +1,43 @@
 Template.univern.rendered = function() {
-}
+},
+
+Template.univern.helpers({
+	getName: function() {
+
+		var currentUser = Meteor.users.findOne();
+
+		if (!currentUser)
+			return;
+
+		return currentUser.profile.name + ' ' + currentUser.profile.surname;
+	},
+	getBaseline: function() {
+
+		var currentUser = Meteor.users.findOne();
+
+		if (!currentUser)
+			return;
+
+		return currentUser.profile.baseline;
+	},
+	getPhoto: function() {
+
+		var currentUser = Meteor.users.findOne();
+
+		if (!currentUser)
+			return;
+
+		return (currentUser.profile.photo.url !== '' ? currentUser.profile.photo.url : false);
+	},
+
+	getVideo: function() {
+
+		var currentUser = Meteor.users.findOne();
+
+		if (!currentUser)
+			return;
+
+		return (currentUser.profile.video !== '' ? currentUser.profile.video : false);
+	}
+})
+
